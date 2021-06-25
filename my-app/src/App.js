f7f99352c2a65bf2a67449e3e662390b24945bdb
Haneen zyad odebat <haneenzyad98@gmail.com>
2021-06-25 15:18:04 +0300

    lab27

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,38 @@ import React from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Form from './components/form.js';
+import People from './People'
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <React.Fragment>
-    <Header/>
-    <Form/>
-    <Footer/>
-  </React.Fragment>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '' , 
+      method: '', 
+      Headers: {} ,
+      Response: {}
+    }
+    };
+  
+  handleForm = (Results) => {
+    this.setState({
+     Results
+    });
+  }
+ 
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <Form handler={this.handleForm} />
+          <People results={this.state} />
+        </main>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
